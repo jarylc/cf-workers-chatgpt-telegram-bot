5cd0ffae037e60dc3bb0dc9bcb09fdd41063a112
Jaryl Chng <git@jarylchng.com>
2023-03-02 19:43:53 +0800

    added additional debug commands and group chat support using `/chatgpt` command

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
 Serverless Telegram bot in webhook mode to quickly interface with [OpenAI's Chat Completion API](https://platform.openai.com/docs/guides/chat)
 
-Currently only usable via 1:1 chat with the bot, group chats are unfortunately not supported.
+Note: This is mainly for personal use, if you would like to add features, do fork the repository. Do perform PRs back if you would be so kind!
 
 ## Prerequisites
 - A Cloudflare account with Workers enabled
@@ -18,8 +18,16 @@ Currently only usable via 1:1 chat with the bot, group chats are unfortunately n
 1. Clone this repository
 2. Run `wrangler secret put TELEGRAM_BOT_TOKEN` and set the Telegram bot token
 3. Run `wrangler secret put OPENAI_API_KEY` and set the OpenAI API key
-4. Add space-delimited usernames to whitelist in `TELEGRAM_USERNAME_WHITELIST` in wrangler.toml
+4. Add space-delimited case sensitive usernames to whitelist in `TELEGRAM_USERNAME_WHITELIST` in wrangler.toml
 5. (Optional) To allow extra lines of context, run `wrangler kv:namespace create kv` and replace the ID of `CHATGPT_TELEGRAM_BOT_KV` and increase `CONTEXT` to more than 0 in wrangler.toml (will consume a lot more tokens)
 6. (Optional) To change the model, update `CHATGPT_MODEL` in wrangler.toml to whatever you want as documented at https://platform.openai.com/docs/api-reference/chat/create#chat/create-model
 7. Run `wrangler publish` to deploy to Cloudflare Workers
 8. Replace `{TELEGRAM_BOT_TOKEN}` and `{WORKERS_NAMESPACE}` on the following `https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url=https%3A%2F%2Fcf-workers-chatgpt-telegram-bot.{WORKERS_NAMESPACE}.workers.dev%2F{TELEGRAM_BOT_TOKEN}` and access it on your browser
+
+## Other Optional Steps
+### Commands list (for BotFather as well)
+```
+start - Start the bot, does nothing otherwise
+chatgpt - Triggers use of the bot in group chats without toggling Private Mode
+context - Shows stored context for the current chat
+```
--- src/index.ts
+++ src/index.ts
@@ -17,24 +17,53 @@ export default {
 	): Promise<Response> {
 		if (request.url.includes(env.TELEGRAM_BOT_TOKEN)) {
 			const update: Telegram.Update = await request.json();
+			console.log(JSON.stringify(update))
 
 			// update is not a message
-			if (!("message" in update)) {
+			if (!("message" in update) || !("text" in update.message)) {
 				return new Response(null); // no action
 			}
+
 			// user is not in whitelist
 			if (env.TELEGRAM_USERNAME_WHITELIST && !env.TELEGRAM_USERNAME_WHITELIST.split(" ").includes(String(update.message.from.username))) {
-				return new Response(null); // no action
+				await fetch("https://api.telegram.org/bot" + env.TELEGRAM_BOT_TOKEN + "/sendMessage", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						"chat_id": update.message.chat.id,
+						"text": "You are not whitelisted!",
+					})
+				});
+				return new Response(null);
 			}
-			// message is /start
-			if (update.message.text == "/start") {
-				return new Response(null); // no action
+
+			// message starts with /start or /chatgpt
+			if (update.message.text.startsWith("/start") || update.message.text.startsWith("/chatgpt")) {
+				await fetch("https://api.telegram.org/bot" + env.TELEGRAM_BOT_TOKEN + "/sendMessage", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						"chat_id": update.message.chat.id,
+						"parse_mode": "Markdown",
+						"reply_markup": {
+							"force_reply": true,
+							"input_field_placeholder": "Ask me anything!",
+							"selective": true,
+						},
+						"text": "Hi @"+ update.message.from.username+"! I'm a chatbot powered by OpenAI! Reply your query to this message!",
+					})
+				});
+				return new Response(null);
 			}
 
 			// process context
 			let context: any = null
 			if (env.CONTEXT && env.CONTEXT > 0 && env.CHATGPT_TELEGRAM_BOT_KV) {
-				context = await env.CHATGPT_TELEGRAM_BOT_KV.get("context")
+				context = await env.CHATGPT_TELEGRAM_BOT_KV.get(String(update.message.chat.id))
 				if (context != null) {
 					context = JSON.parse(context)
 				}
@@ -42,12 +71,30 @@ export default {
 			if (context == null) {
 				context = []
 			}
+
+			// message starts with /context
+			if (update.message.text.startsWith("/context")) {
+				await fetch("https://api.telegram.org/bot" + env.TELEGRAM_BOT_TOKEN + "/sendMessage", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						"chat_id": update.message.chat.id,
+						"parse_mode": "Markdown",
+						"text": JSON.stringify(context),
+					})
+				});
+				return new Response(null);
+			}
+
+			// prepare context
 			context.push({"role": "user", "content": update.message.text})
 			while (context.length > Math.max(1, env.CONTEXT*2)) {
 				context.shift();
 			}
 
-			// query OpenAPI
+			// query OpenAPI with context
 			const response = await fetch("https://api.openai.com/v1/chat/completions", {
 				method: "POST",
 				headers: {
@@ -70,7 +117,7 @@ export default {
 					"Content-Type": "application/json",
 				},
 				body: JSON.stringify({
-					"chat_id": update.message.from.id,
+					"chat_id": update.message.chat.id,
 					"parse_mode": "Markdown",
 					"text": content
 				})
@@ -79,7 +126,7 @@ export default {
 			// update context
 			if (env.CONTEXT && env.CONTEXT > 0 && env.CHATGPT_TELEGRAM_BOT_KV) {
 				context.push({"role": "assistant", "content": content})
-				await env.CHATGPT_TELEGRAM_BOT_KV.put("context", JSON.stringify(context))
+				await env.CHATGPT_TELEGRAM_BOT_KV.put(String(update.message.chat.id), JSON.stringify(context))
 			}
 
 			return new Response(null);
